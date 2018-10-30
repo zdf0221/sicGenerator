26e448a884d30ae6b3bf47b7302f468901717a52
zdf0221 <zdf0221@users.noreply.github.com>
2018-10-30 18:50:55 -0400

    + add HTML CSS JS code
    JS running is in trouble, double-check NEEDED!

--- generator/urls.py
+++ generator/urls.py
@@ -10,11 +10,11 @@
                    2018/10/29:20:07
 -------------------------------------------------
 """
-
+from django.conf import settings
 from django.urls import path
-
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', views.index),
 ]
--- generator/views.py
+++ generator/views.py
@@ -6,4 +6,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, "index.html",)
+    return render(request, "index.html")
