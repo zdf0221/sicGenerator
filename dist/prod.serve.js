446a8bd6ffb5661edf565a6e8f9de06210715b82
FTDRefrain <yuanzhenyi@sjtu.edu.cn>
2018-10-31 10:47:16 -0400

    Add files via upload

---
+++ dist/prod.serve.js
@@ -0,0 +1,25 @@
+/**
+ * Created by adminpc on 10/31/18.
+ */
+var express = require('express')
+var config = require('./config/index')
+
+var port = process.env.PORT || config.build.port
+var app = express()
+
+var router = express.Router()
+
+router.get('/', function (req, res, next) {
+  req.url = '/index.html'
+  next();
+})
+
+app.use(express.static('../dist'))
+
+module.exports = app.listen(port, function (err) {
+  if(err){
+    console.log(err)
+    return
+  }
+  console.log('start listen')
+})
