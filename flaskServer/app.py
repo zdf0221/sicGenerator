94b6ca5d1bf82082cc16a98dd5c721a61e3ba8a4
zdf0221 <zdf0221@users.noreply.github.com>
2018-11-07 23:15:16 -0500

    modified output
    * modified output format
    * used json.dumps to create dict file
    ! bug report, switching different major will kill the generating button, need fix

--- flaskServer/app.py
+++ flaskServer/app.py
@@ -1,22 +1,26 @@
 from flask import Flask, render_template, request, redirect
-from flask_cors import CORS
+# from flask_cors import CORS
 import json
 
 app = Flask(__name__, static_url_path='/static')
-CORS(app)
+# CORS(app)
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'use //generator to enter demo'
 
 
 @app.route('/generator')
 def generator():
     return render_template('index.html')
 
+
 @app.route('/apiGiven', methods=['POST'])
 def apiGiven():
         data = request.json
+        print(json.dumps(data, sort_keys=True, indent=4, separators=(',', ':')))
+
         return json.dumps(data)
 
 
